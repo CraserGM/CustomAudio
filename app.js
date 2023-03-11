cde04711df59fb6024dcd483d50f68ac5bfcf9af
Craser <hernaogabriel@gmail.com>
2023-03-10 20:50:33 -0400

    comienzo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const audio = new Audio('I-Am-Just-A-Freak.mp3')
+const audio = document.createElement("audio")
+const audioPlay = document.querySelector(".play")
+const audioPause = document.querySelector(".pause")
+
+audio.src = "audio/I-Am-Just-A-Freak.mp3"
+audio.preload = "metadata"
+audio.loop = true
+document.body.appendChild(audio)
+
+audioPlay.addEventListener("click", () =>{
+    audio.play()
+    document.querySelector(".play").classList.toggle("hide")
+    document.querySelector(".pause").classList.toggle("hide")
+})
+audioPause.addEventListener("click", () =>{
+    audio.pause()
+    document.querySelector(".play").classList.toggle("hide")
+    document.querySelector(".pause").classList.toggle("hide")
+})
